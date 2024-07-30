eef3abc75dd05da810d64e31e4747e49bbc4ed69
Beniamino-Soccio <beniamino43@gmail.com>
2024-07-30 14:57:21 +0200

    analisi traccia + prova collegamento script.js

---
+++ java script/script.js
@@ -0,0 +1,14 @@
+// traccia: chiedere all'utente il nome, il cognome, il suo colore preferito e stampare in console il tutto
+
+/* Analisi dei passaggi
+1.Recuperare gli elementi che mi interessano dal DOM (in questo caso solo durante il BONUS prima di inserire il risultato nell'elemento del DOM)
+2.preparo eventuali variabili da tenere in considerazione fin da subito
+3.chiedere l'età all'utente
+4.chiedere il suo cognome
+5.chiedere il suo colore preferito
+6. stampo nella console la password
+7.BONUS: stampo la password anche nell'elemento del DOM
+*/
+
+// prova collegamento file .js
+console.log('JS OK')
