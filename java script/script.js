113ba0d703b3001d32528512d36a4819687e8e67
Beniamino-Soccio <beniamino43@gmail.com>
2024-07-30 15:07:26 +0200

    added variable passwordNumbers and fixed analysis

--- java script/script.js
+++ java script/script.js
@@ -3,7 +3,7 @@
 /* Analisi dei passaggi
 1.Recuperare gli elementi che mi interessano dal DOM (in questo caso solo durante il BONUS prima di inserire il risultato nell'elemento del DOM)
 2.preparo eventuali variabili da tenere in considerazione fin da subito
-3.chiedere l'età all'utente
+3.chiedere il nome all'utente
 4.chiedere il suo cognome
 5.chiedere il suo colore preferito
 6. stampo nella console la password
@@ -12,3 +12,6 @@
 
 // prova collegamento file .js
 console.log('JS OK')
+
+//preparo eventuali variabili da tenere in considerazione fin da subito
+const passwordNumbers = '21'; // perchè ho pensato che il numero 21 sia stato prefissato che fosse quello.
